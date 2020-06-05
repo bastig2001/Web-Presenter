4d86a6809048f46455f283c3c6bd9fcff7910e18
Sebastian Grman <sebastian.grman@gmail.com>
2020-06-05 15:16:39 +0200

    removed end method in RunningPresentationFundamentals

--- WebPresenter/ClientApp/src/app/types/runningPresentationFundamentals.ts
+++ WebPresenter/ClientApp/src/app/types/runningPresentationFundamentals.ts
@@ -1,14 +1,5 @@
 import {PresentationFundamentals} from "./presentationFundamentals";
-import {HttpClient} from "@angular/common/http";
 
 export class RunningPresentationFundamentals extends PresentationFundamentals {
   public id: string;
-
-  public end(http: HttpClient, callback: (successful: boolean, error: Error) => void) {
-    http.delete(`/data/presentations/${this.id}`)
-      .subscribe(
-        () => callback(true, null),
-        error => callback(false, error)
-      );
-  }
 }
