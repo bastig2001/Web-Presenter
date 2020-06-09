18e1b59df217af3797afd0def229bc66f670c056
Sebastian Grman <sebastian.grman@gmail.com>
2020-06-09 14:54:33 +0200

    changed formatting in program

--- WebPresenter/Program.cs
+++ WebPresenter/Program.cs
@@ -15,6 +15,8 @@ namespace WebPresenter {
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+                .ConfigureWebHostDefaults(webBuilder => {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
